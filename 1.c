94d32c0b2b813a953b6d57081d5637eba9158613
Nika Ramin <128173488+NikaRamin@users.noreply.github.com>
2025-09-14 01:05:15 +0330

    Update 1.c

--- 1.c
+++ 1.c
@@ -3,70 +3,39 @@
 
 void main(void)
 {
-    unsigned char led_position = 0;
-    unsigned char active_bargraph = 0;
-    unsigned char led_states[3] = {0x00, 0x00, 0x00};
-
     DDRA = 0xFF;
     DDRB = 0xFF;
     DDRC = 0xFF;
-    DDRD = 0x00;
-
-    PORTA = led_states[0];
-    PORTB = led_states[1];
-    PORTC = led_states[2];
+    DDRD = 0xFF;
 
     while (1)
     {
-        if (PIND.0 == 1) { // UP
-            if (active_bargraph > 0) {
-                active_bargraph--;
-            }
-            delay_ms(200);
+        int i;
+
+        PORTA = 0xFF;
+        delay_ms(310);
+        PORTA = 0x00;
+        delay_ms(310);
+
+        for(i=7; i>=0; i--)
+        {
+            PORTB = (1<<i);
+            delay_ms(45);
         }
+        PORTB = 0x00;
 
-        if (PIND.1 == 1) { // DOWN
-            if (active_bargraph < 2) {
-                active_bargraph++;
-            }
-            delay_ms(200);
+        for(i=0; i<8; i++)
+        {
+            PORTC = (1<<i);
+            delay_ms(260);
         }
+        PORTC = 0x00;
 
-        if (PIND.2 == 1) { // LEFT
-            if (led_position > 0) {
-                led_position--;
-            }
-            delay_ms(200);
+        for(i=0; i<8; i++)
+        {
+            PORTD = (1<<i);
+            delay_ms(110);
         }
-
-        if (PIND.3 == 1) { // RIGHT
-            if (led_position < 7) {
-                led_position++;
-            }
-            delay_ms(200);
-        }
-
-        if (PIND.4 == 1) { // TOGGLE
-            led_states[active_bargraph] ^= (1 << led_position);
-            delay_ms(200);
-        }
-
-        PORTA = led_states[0];
-        PORTB = led_states[1];
-        PORTC = led_states[2];
-
-        // Blinking cursor to show selected LED
-        switch(active_bargraph) {
-            case 0: PORTA ^= (1 << led_position); break;
-            case 1: PORTB ^= (1 << led_position); break;
-            case 2: PORTC ^= (1 << led_position); break;
-        }
-        delay_ms(150);
-        
-        // Restore original state
-        PORTA = led_states[0];
-        PORTB = led_states[1];
-        PORTC = led_states[2];
-
+        PORTD = 0x00;
     }
 }
