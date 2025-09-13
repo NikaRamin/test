bb804c227218e2b9bff9d662fe6455fea16f1fb6
Nika Ramin <128173488+NikaRamin@users.noreply.github.com>
2025-09-14 01:08:27 +0330

    Update 1.c

--- 1.c
+++ 1.c
@@ -17,20 +17,23 @@ void main(void)
         PORTA = 0x00;
         delay_ms(310);
 
-        for(i=7; i>=0; i--)
+        // Corrected Port B: Downward Pattern
+        for(i=0; i<8; i++)
         {
             PORTB = (1<<i);
             delay_ms(45);
         }
         PORTB = 0x00;
 
-        for(i=0; i<8; i++)
+        // Corrected Port C: Upward Pattern
+        for(i=7; i>=0; i--)
         {
             PORTC = (1<<i);
             delay_ms(260);
         }
         PORTC = 0x00;
-
+        
+        // Port D: One by one
         for(i=0; i<8; i++)
         {
             PORTD = (1<<i);
